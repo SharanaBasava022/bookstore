f29141552d095c9419d9130fc697460fd3a5b0e9
shar bas <sbasava022@gmail.com>
2022-06-23 12:14:54 +0530

    hhhh

---
+++ backend/routes/book-routes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const booksController = require("../controllers/books-controllers");
+
+router.get("/books", booksController.getAllBooks);
+router.post("/add", booksController.addBook);
+router.get("/books/:id", booksController.getById);
+router.put("/books/:id", booksController.updateBook);
+router.delete("/books/:id", booksController.deleteBook);
+
+module.exports = router;
